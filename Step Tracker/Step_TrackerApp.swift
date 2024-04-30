c82010114ac3e13da2a428f0319e4bb9320c8b22
5mars <101134920+5mars@users.noreply.github.com>
2024-04-29 22:42:09 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jeremy Cinq-Mars on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
